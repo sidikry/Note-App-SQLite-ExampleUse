8588dd93abfd1d4e2365d43746522245b47f057d
sidikry <sidik.14@students.amikom.ac.id>
2020-06-30 10:03:47 +0700

    create a function in noteprovider

--- app/src/main/java/com/timkontrakan/mynotesapp/provider/NoteProvider.java
+++ app/src/main/java/com/timkontrakan/mynotesapp/provider/NoteProvider.java
@@ -2,17 +2,57 @@ package com.timkontrakan.mynotesapp.provider;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
+import com.timkontrakan.mynotesapp.db.NoteHelper;
+
+import static android.media.tv.TvContract.AUTHORITY;
+import static android.provider.CallLog.Calls.CONTENT_URI;
+import static com.timkontrakan.mynotesapp.db.DatabaseContract.NoteColumns.TABLE_NAME;
+
 public class NoteProvider extends ContentProvider {
-    public NoteProvider() {
+
+    private static final int NOTE = 1;
+    private static final int NOTE_ID = 2;
+    private NoteHelper noteHelper;
+
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+
+        sUriMatcher.addURI(AUTHORITY, TABLE_NAME, NOTE);
+
+        sUriMatcher.addURI(AUTHORITY,
+                TABLE_NAME + "/#",
+                NOTE_ID);
     }
 
     @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
-        // Implement this to handle requests to delete one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public boolean onCreate() {
+        noteHelper = NoteHelper.getInstance(getContext());
+        noteHelper.open();
+        // TODO: Implement this to initialize your content provider on startup.
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        Cursor cursor;
+        switch (sUriMatcher.match(uri)) {
+            case NOTE:
+                cursor = noteHelper.queryAll();
+                break;
+            case NOTE_ID:
+                cursor = noteHelper.queryById(uri.getLastPathSegment());
+                break;
+            default:
+                cursor = null;
+                break;
+        }
+        return cursor;
     }
 
     @Override
@@ -24,27 +64,51 @@ public class NoteProvider extends ContentProvider {
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        // TODO: Implement this to handle requests to insert a new row.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    @Override
-    public boolean onCreate() {
-        // TODO: Implement this to initialize your content provider on startup.
-        return false;
-    }
-
-    @Override
-    public Cursor query(Uri uri, String[] projection, String selection,
-                        String[] selectionArgs, String sortOrder) {
-        // TODO: Implement this to handle query requests from clients.
-        throw new UnsupportedOperationException("Not yet implemented");
+        long added;
+        switch (sUriMatcher.match(uri)) {
+            case NOTE:
+                added = noteHelper.insert(values);
+                break;
+            default:
+                added = 0;
+                break;
+        }
+        getContext().getContentResolver().notifyChange(CONTENT_URI, null);
+        return Uri.parse(CONTENT_URI + "/" + added);
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection,
                       String[] selectionArgs) {
-        // TODO: Implement this to handle requests to update one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+        int updated;
+        switch (sUriMatcher.match(uri)) {
+            case NOTE_ID:
+                updated = noteHelper.update(uri.getLastPathSegment(), values);
+                break;
+            default:
+                updated = 0;
+                break;
+        }
+        getContext().getContentResolver().notifyChange(CONTENT_URI, null);
+        return updated;
     }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        int deleted;
+        switch (sUriMatcher.match(uri)) {
+            case NOTE_ID:
+                deleted = noteHelper.deleteById(uri.getLastPathSegment());
+                break;
+            default:
+                deleted = 0;
+                break;
+        }
+        getContext().getContentResolver().notifyChange(CONTENT_URI, null);
+        return deleted;
+    }
+
+    public NoteProvider() {
+    }
+
 }
